a83193cb88691f0e46970b691f686a1f1e62f46a
shaka <jaherm9@gmail.com>
2021-10-25 00:26:51 +0600

    env local added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import './App.css';
+import Booking from './Pages/Booking/Booking';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound';
+import Header from './Pages/Shared/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+
+        {/* Dynamic route */}
+        <Route path="/booking/:serviceId">
+          <Booking></Booking>
+        </Route>
+
+
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
